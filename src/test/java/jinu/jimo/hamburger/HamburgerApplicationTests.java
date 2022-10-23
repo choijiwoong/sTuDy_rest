9726c5b89d4595b5be0495ebd92b064e526d2ec8
Choijiwoong <gogogi313@naver.com>
2022-10-23 17:25:03 +0900

    [DOCS] 스켈레톤 파일 업로드

---
+++ src/test/java/jinu/jimo/hamburger/HamburgerApplicationTests.java
@@ -0,0 +1,13 @@
+package jinu.jimo.hamburger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HamburgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
